0c02b5998719843d2f89c3c0bc45240105b4be04
V.Vitaliy <vitaliyverzyn25@gmail.com>
2024-03-13 21:05:12 +0200

    first commit - project initialization

---
+++ src/main/java/com/spring/web/service/project/SpringRestWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.web.service.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestWebProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/web/service/project/SpringRestWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.web.service.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
